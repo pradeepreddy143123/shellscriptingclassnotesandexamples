55c154869db12cb1040b142b35204b07fbd0be72
Amrutha Raavi <raaviamrutha62@gmail.com>
2020-11-22 17:47:50 +0000

    Update 9.sh

--- shell_scripting-20-nov/9.sh
+++ shell_scripting-20-nov/9.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 #Write a shell script that receives any number of file names as arguments checks if every argument supplied is a file or a directory and reports accordingly. Whenever the argument is a file, the number of lines on it is also reported.
-	
+if [ $# -eq 1 ];then	
 for file in $*
 do
 	if [ -f $file ];then
@@ -14,4 +14,6 @@ do
 		echo "please enter valid file or directory name"
 	fi
 done
-echo "usage $0:  <file_or_dir> [option] [option]"
+else
+echo "usage $0:  <file_or_dir> [<file_name>..]"
+fi
