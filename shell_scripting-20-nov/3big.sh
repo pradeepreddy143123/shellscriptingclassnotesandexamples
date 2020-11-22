7d525889bfee61596501fd43c3890579b082f934
Amrutha Raavi <raaviamrutha62@gmail.com>
2020-11-22 16:37:57 +0000

    biggest of 3 numbers

---
+++ shell_scripting-20-nov/3big.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+#Write Script to find out biggest number from given three nos. Nos are supplied as command line arguments. Print error if sufficient arguments are not supplied.
+
+echo "1st no $1"
+echo "2nd no $2"
+echo "3rd no $3"
+ 
+if [ $# -eq 3 ]
+then
+	if [ $1 -gt $2 ] && [ $1 -gt $3 ];then
+		echo "$1 is bigger"
+	elif [ $2 -gt $1 ] && [ $2 -gt $3 ];then
+		echo "$2 is greater"
+	else
+		echo "$3 is bigger"
+	fi
+else
+	echo "$0 : Required <arg1> <arg2> <arg3>"
+fi
