ce105a26b28e9235ae017d544cb5c404c3c3f913
Amrutha Raavi <raaviamrutha62@gmail.com>
2020-11-23 02:31:11 +0000

    Update add2num.sh

--- shell_scripting-20-nov/add2num.sh
+++ shell_scripting-20-nov/add2num.sh
@@ -2,8 +2,8 @@
 
 #write shell script that will add two nos, which are supplied as command line argument, and if this two nos are not given show error and its usage
 
-echo "shell 1st argument is \$1 : $1"
-echo "shell 2nd argument is \$2 : $2"
+#echo "shell 1st argument is \$1 : $1"
+#echo "shell 2nd argument is \$2 : $2"
 if [ $# -ne 2 ];then
 
 	echo "usage :$0 <arg1> <arg2>"
