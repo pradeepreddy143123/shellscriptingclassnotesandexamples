3a76a39cd8c8bff00b4b543f8f3f5f551a151c04
Kotapati Bhargav <bhargavkotapati9912@gmail.com>
2021-10-21 10:38:51 +0000

    Update 6-lines_between_start_and_end_numbers.sh

--- Shell Programming/Lab Assignments1/6-lines_between_start_and_end_numbers.sh
+++ Shell Programming/Lab Assignments1/6-lines_between_start_and_end_numbers.sh
@@ -1,3 +1,7 @@
+<<question
+6. Write a shell script that accepts a file name, starting and ending line numbers as arguments and displays all the lines between the given line numbers.
+question
+
 #!/bin/sh
 
 
