093e7953f15eeb662e723b0b5dddf2a0e1437c5e
Kotapati Bhargav <bhargavkotapati9912@gmail.com>
2021-10-21 11:56:19 +0000

    Deleted Shell Programming/Lab Assignments1/.gitkeep, Shell Programming/Lab Assignments1/1-Addition_of_two_numbers.sh, Shell Programming/Lab Assignments1/2-Biggest_among_three_numbers.sh, Shell Programming/Lab Assignments1/3-reverse_of_a_number.sh, Shell Programming/Lab Assignments1/4-whether_file_exists_or_not.sh, Shell Programming/Lab Assignments1/5-printing_message_in_different_formats.sh, Shell Programming/Lab Assignments1/6-lines_between_start_and_end_numbers.sh, Shell Programming/Lab Assignments1/7-deletion_of_lines_containig_words.sh, Shell Programming/Lab Assignments1/8-checking_for_read_write_and_executable.sh, Shell Programming/Lab Assignments1/9-counting_no_of_lines_in_a_file.sh, Shell Programming/Lab Assignments1/A-count_of_each_word_in_first_file_in_other_files.sh, Shell Programming/Lab Assignments1/B-directory_files_in_a_directory.sh, Shell Programming/Lab Assignments1/C-factorial_of_a_number.sh, Shell Programming/Project Work/.gitkeep, Shell Programming/Project Work/server_monitor.sh, Shell Programming/.gitkeep files

--- Shell Programming/Lab Assignments1/7-deletion_of_lines_containig_words.sh
+++
@@ -1,9 +0,0 @@
-<<question
-7. Write a shell script that deletes all lines containing a specified word in one or more files supplied as arguments to it.
-question
-
-#!/bin/sh
-
-echo "enter the word"
-read word
-cat $@ | grep -v $word 
