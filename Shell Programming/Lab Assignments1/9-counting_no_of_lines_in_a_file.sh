deeb7b952a5178389549893feb7d949a5561ca48
Kotapati Bhargav <bhargavkotapati9912@gmail.com>
2021-10-21 10:41:53 +0000

    Update 9-counting_no_of_lines_in_a_file.sh

--- Shell Programming/Lab Assignments1/9-counting_no_of_lines_in_a_file.sh
+++ Shell Programming/Lab Assignments1/9-counting_no_of_lines_in_a_file.sh
@@ -1,3 +1,8 @@
+<<question
+9. Write a shell script that receives any number of file names as arguments checks if every argument supplied is a file or a directory and reports accordingly. 
+Whenever the argument is a file, the number of lines on it is also reported.
+question
+
 #!/bin/sh
 
 for VAR in $@
