babdb159a0c64623a45fa7e210a023c7fb09b028
Kotapati Bhargav <bhargavkotapati9912@gmail.com>
2021-10-21 10:34:23 +0000

    Upload New File

---
+++ Shell Programming/Lab Assignments1/5-printing_message_in_different_formats.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo -n "\033[31m\033[1mHello \033[32m\033[5mWorld"
+echo "\033[0m"
+#echo "\033[33mHello World"
+#echo -n "\033[33mHello World"
+#echo "\033[0m"
+#echo "\033[01mHello World"
+#echo -n "\033[05mHello World"
+#echo "\033[0m"
