be4ae7a3893d137e9f0157326ca0d45a2e6ac329
Kotapati Bhargav <bhargavkotapati9912@gmail.com>
2021-10-21 10:40:46 +0000

    Update 8-checking_for_read_write_and_executable.sh

--- Shell Programming/Lab Assignments1/8-checking_for_read_write_and_executable.sh
+++ Shell Programming/Lab Assignments1/8-checking_for_read_write_and_executable.sh
@@ -1,3 +1,7 @@
+<<question
+8. Write a shell script that displays a list of all the files in the current directory to which the user has read, write and execute permissions.
+question
+
 #!/bin/sh
 
 
