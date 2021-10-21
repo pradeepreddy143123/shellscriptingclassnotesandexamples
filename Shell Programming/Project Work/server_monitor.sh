e0dbad50265a3ca0b12b45dedd92f8288c4bd99d
Kotapati Bhargav <bhargavkotapati9912@gmail.com>
2021-10-21 10:48:38 +0000

    Update server_monitor.sh

--- Shell Programming/Project Work/server_monitor.sh
+++ Shell Programming/Project Work/server_monitor.sh
@@ -1,3 +1,19 @@
+<<question
+Mini Project - shell programming:
+
+Write a network monitor application server_monitor.sh which show the below information
+
+OS name
+Architecture version
+Kernel version
+Internet status
+IP Address
+Memory Usage
+Disk file systems usage
+system uptime
+
+question
+
 #!/bin/sh
 
 echo ""
