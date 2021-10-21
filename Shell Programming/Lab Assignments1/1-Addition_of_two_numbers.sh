2eb50758c5fdbac8c8c935c3a382afa0dd4e65ea
Kotapati Bhargav <bhargavkotapati9912@gmail.com>
2021-10-21 10:30:49 +0000

    Update 1-Addition_of_two_numbers.sh

--- Shell Programming/Lab Assignments1/1-Addition_of_two_numbers.sh
+++ Shell Programming/Lab Assignments1/1-Addition_of_two_numbers.sh
@@ -1,3 +1,7 @@
+<<comment
+1. Write shell script that will add two nos, which are supplied as command line argument, and if this two nos are not given show error and its usage.
+comment
+
 #!/bin/sh
 
 if [ $# -eq 2 ]
