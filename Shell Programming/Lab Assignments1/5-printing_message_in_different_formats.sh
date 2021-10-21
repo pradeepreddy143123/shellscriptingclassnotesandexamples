5201634472cf6ce3e4f917520c315f50e9faf1c9
Kotapati Bhargav <bhargavkotapati9912@gmail.com>
2021-10-21 10:36:06 +0000

    Update 5-printing_message_in_different_formats.sh

--- Shell Programming/Lab Assignments1/5-printing_message_in_different_formats.sh
+++ Shell Programming/Lab Assignments1/5-printing_message_in_different_formats.sh
@@ -1,10 +1,16 @@
+<<question
+5. How to write script, that will print, Message ""Hello World"", in Bold and Blink effect, and in different Colors like red, brown etc using echo command."
+question
+
 #!/bin/sh
 
-echo -n "\033[31m\033[1mHello \033[32m\033[5mWorld"
+echo -n "\033[31m\033[1mHello \033[33m\033[5mWorld"
 echo "\033[0m"
-#echo "\033[33mHello World"
-#echo -n "\033[33mHello World"
-#echo "\033[0m"
-#echo "\033[01mHello World"
-#echo -n "\033[05mHello World"
-#echo "\033[0m"
+<<comment
+echo "\033[33mHello World"
+echo -n "\033[33mHello World"
+echo "\033[0m"
+echo "\033[01mHello World"
+echo -n "\033[05mHello World"
+echo "\033[0m"
+comment
