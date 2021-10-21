65c306a4d049e26923f105ba08b351a3952463bc
Meka Satya Vamsi <msvamsy@gmail.com>
2021-10-21 14:26:22 +0000

    Update ShellScripting_Assignments/5.sh

---
+++ ShellScripting_Assignments/5.sh
@@ -0,0 +1,13 @@
+# How to write script, that will print, Message ""Hello World"", in Bold and Blink effect, and in different Colors like red, brown etc using echo command."
+
+#!/bin/sh
+
+echo  "\033[1m Hello World"
+
+echo  "\033[5m Hello world"
+
+echo  "\033[0m Hello World"
+
+echo  "\033[31m Hello World"
+
+echo  "\033[35m Hello World"
