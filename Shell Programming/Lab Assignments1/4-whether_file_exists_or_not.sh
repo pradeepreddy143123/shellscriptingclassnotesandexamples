090f61b8258264ba14e663b79dad4f23312912bb
Kotapati Bhargav <bhargavkotapati9912@gmail.com>
2021-10-21 10:33:31 +0000

    Update 4-whether_file_exists_or_not.sh

--- Shell Programming/Lab Assignments1/4-whether_file_exists_or_not.sh
+++ Shell Programming/Lab Assignments1/4-whether_file_exists_or_not.sh
@@ -1,3 +1,7 @@
+<<comment
+4. Write script to determine whether given file exist or not, file name is supplied as command line argument, Also check for sufficient number of command line arguments.
+comment
+
 #!/bin/sh
 
 if [ $# -eq 1 ]
